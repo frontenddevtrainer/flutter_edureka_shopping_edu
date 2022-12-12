d045dacf46b4997382a7609c28efc75a64bd7109
Admin <admin@example.com>
2022-12-12 15:54:13 +0530

    Stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,17 @@ void main() {
   runApp(const EdurekaShopApp());
 }
 
-class EdurekaShopApp extends StatelessWidget {
+class EdurekaShopApp extends StatefulWidget {
   const EdurekaShopApp({super.key});
 
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return EdurekaShopAppState();
+  }
+}
+
+class EdurekaShopAppState extends State<EdurekaShopApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,8 +23,25 @@ class EdurekaShopApp extends StatelessWidget {
         title: const Text("Edureka Shop"),
       ),
       body: Column(children: [
-        Row(children: const [Text("hello"), Text("World"), ElevatedButton(onPressed: null, child: Text("Login"))],),
-         Row(children: const [Text("hello2"), Text("World2"), ElevatedButton(onPressed: null, child: Text("Register"))],)
+        Row(
+          children: const [
+            Text("hello"),
+            Text("World"),
+            ElevatedButton(onPressed: null, child: Text("Login"))
+          ],
+        ),
+        Row(
+          children: const [
+            Text("hello2"),
+            Text("World2"),
+            ElevatedButton(onPressed: null, child: Text("Register"))
+          ],
+        ),
+        Row(
+          children: const [
+            Text(""),
+          ],
+        )
       ]),
     ));
   }
